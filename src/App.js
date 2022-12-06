6682b81a6e322c1cf884d4907d5284e743f53906
Sherzod Yoqubov <yoqubovsherzod1997@gmail.com>
2022-12-06 21:07:41 +0500

    .

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './styles/app-style.css'
+import React from 'react'
+import TableList from './components/TableList';
+
+
+function App() {
+  return (
+    <div className="app-style">
+      <TableList/>
+    </div>
+  );
+}
+
+export default App;
